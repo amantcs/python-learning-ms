d44feaca490c64ec844148e29ce019a0cc08a95a
Amandeep Singh <samandeep540@gmail.com>
2023-07-24 21:22:37 +0530

    Calculator

--- 4.excercise1.py
+++ 4.excercise1.py
@@ -2,7 +2,8 @@ from datetime import date
 
 print(date.today())
 
-parsec = 11
-lightyears = parsec*3.26
+parsec = input("Enter Parsec: ")
+
+lightyears = int(parsec)*3.26
 
 print(str(parsec) + " parsecs is " + str(lightyears)  + " lightyears")
---
+++ 6.calculator.py
@@ -0,0 +1,3 @@
+first_number = input("Enter First Number: ")
+second_number = input("Enter Second Number: ")
+print(int(first_number) + int(second_number))
